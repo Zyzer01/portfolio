dd470afd99fa105ec978aba3230fad9e6f61a9d2
David Onifade <davidicfola@gmail.com>
2023-10-23 23:01:33 +0000

    Set up

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div className="flex">
+        <h1>Hello, I am David How are you</h1>
+    </div>
+  );
+};
